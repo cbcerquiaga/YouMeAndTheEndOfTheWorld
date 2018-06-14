57fe1153115c3011b0d0b3f70d29c22c291cfca2
Daniel Zwiener <17934820+UngratefulFool@users.noreply.github.com>
2018-06-14 18:55:00 -0500

    Started to add saving, but require a little more information before continuing #48

--- You, Me, and the End of the World/Scripts/sceneManager.gd
+++ You, Me, and the End of the World/Scripts/sceneManager.gd
@@ -122,3 +122,58 @@ func update_camera():
 #	print(canvas_transform.get_scale())
 #	print(maxValue)
 	
+#func save():
+#	var save_dict = {
+#		"filename" : get_filename(),
+#		"parent" : get_parent().get_path(),
+#		"p1Pos_x" : player1.position.x,
+#		"p1Pos_y" : player1.position.y,
+#		"p2Pos_x" : player2.postion.x,
+#		"p2Pos_y" : player2.position.y,
+#		"p1Properties" : player1.playerProperties,
+#		"p2Properties" : player2.playerProperties
+#	}
+#	return save_dict
+#
+#func save_game():
+#	var save_game = File.new()
+#	save_game.open("user://savegame.save", File.WRITE)
+#	var save_nodes = get_tree().get_nodes_in_group("Persist")
+#	for i in save_nodes:
+#		var node_data = i.call("save");
+#		save_game.store_line(to_json(node_data))
+#	save_game.close()
+#
+## Note: This can be called from anywhere inside the tree. This function is path independent.
+#func load_game():
+#    var save_game = File.new()
+#    if not save_game.file_exists("user://save_game.save"):
+#        return # Error! We don't have a save to load.
+#
+#    # We need to revert the game state so we're not cloning objects during loading. This will vary wildly depending on the needs of a project, so take care with this step.
+#    # For our example, we will accomplish this by deleting savable objects.
+#    var save_nodes = get_tree().get_nodes_in_group("Persist")
+#    for i in save_nodes:
+#        i.queue_free()
+#
+#    # Load the file line by line and process that dictionary to restore the object it represents
+#    save_game.open("user://savegame.save", File.READ)
+#    while not save_game.eof_reached():
+#        var current_line = parse_json(save_game.get_line())
+#        # First we need to create the object and add it to the tree and set its position.
+#        var new_object = load(current_line["filename"]).instance()
+#        get_node(current_line["parent"]).add_child(new_object)
+#        new_object.position = Vector2(current_line["pos_x"], current_line["pos_y"]))
+#        # Now we set the remaining variables.
+#        for i in current_line.keys():
+#            if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
+#                continue
+#            new_object.set(i, current_line[i])
+#    save_game.close()
+
+
+
+
+
+
+
