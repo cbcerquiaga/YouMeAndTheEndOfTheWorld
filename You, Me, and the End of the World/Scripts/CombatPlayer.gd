cd9012c09ede9657b18f6422e6ff257d3ac7c62f
Blake Erquiaga <cbcerquiaga@me.com>
2018-06-21 12:21:50 -0400

    Added Combat Stuff
    Added the framework for the combat scene. I added tentative controls to the CombatPlayer script, and I made an ammo indicator for a 5-shooter revolver. I'll need to make more depending on how much weapon variety we want to have.

---
+++ You, Me, and the End of the World/Scripts/CombatPlayer.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+	if Input.is_action_pressed("p1_move_up"):
+		motion += Vector2(0, -1)
+	if Input.is_action_pressed("p1_move_bottom"):
+		motion += Vector2(0, 1)
+	if Input.is_action_pressed("p1_move_left"):
+		motion += Vector2(-1, 0)
+	if Input.is_action_pressed("p1_move_right"):
+		motion += Vector2(1, 0)
+	if Input.is_action_pressed("p2_move_left"):#A
+		print("grab")
+	if Input.is_action_pressed("p2_action2"):#E
+		print("head melee attack")
+	if Input.is_action_pressed("p2_move_right"):#D
+		print("body melee attack")
+	if Input.is_action_pressed("p2_move_up"):#W
+		print("head block")
+	if Input.is_action_pressed("p2_move_down"):#S
+		print("body block")
+	if Input.is_action_pressed("ui_select"):#spacebar
+		print("heavy melee attack")
+	if Input.is_action_pressed("F key"):#F
+		print("lunge attack")
+	pass
