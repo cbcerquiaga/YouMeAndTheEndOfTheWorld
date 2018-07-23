cb753158867e634447ca5ebc0152594c70c53672
Blake Erquiaga <cbcerquiaga@me.com>
2018-07-23 11:49:56 -0400

    Added Fishing Scene
    I know we're still not done with combat or exploring, but I'm stuck on those and want to make progress someplace. Still a lot of work to be done.

---
+++ You, Me, and the End of the World/Scripts/FishingPlayer1.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+const SPEED = 1
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+	var left = Input.is_action_pressed("p1_move_left")
+	var right = Input.is_action_pressed("p1_move_right")
+	var up = Input.is_action_pressed("p1_move_up")
+	var down = Input.is_action_pressed("p1_move_down")
+	
+	if left:
+		velocity -= Vector2(SPEED,0)
+		pass
+	elif right:
+		velocity += Vector2(SPEED,0)
+		pass
+	elif down:
+		velocity += Vector2(0,SPEED)
+		pass
+	elif up:
+		velocity -= Vector2(0,SPEED)
+		pass
+		
+	if motion != Vector2(0,0):
+		move_and_slide(motion)
+		emit_signal("move")
+	pass
