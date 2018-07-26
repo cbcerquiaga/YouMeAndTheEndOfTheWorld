e4cf788584cf6331dfff3ffdf83de5fa83b379ad
Blake Erquiaga <cbcerquiaga@me.com>
2018-07-26 11:21:57 -0400

    Buttons buttons buttons
    I added stuff to the combat script to make the run away and surrender buttons work, and also got rid of the unnecessary node setup I had where the sprite and button were separated. For some reason, the run away button works but the surrender button does not. Maybe @ungratefulfool you can figure out why but they look identical to me. Will be doing the same for other UI buttons soon.

--- You, Me, and the End of the World/Scripts/CombatSceneManager.gd
+++ You, Me, and the End of the World/Scripts/CombatSceneManager.gd
@@ -3,11 +3,30 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 var crosshair = load("res://Images/Crosshair.png")
+#export (NodePath) var button_path
+onready var surrenderButton = get_node("CombatHUD/Surrender Button")
+onready var runButton = get_node("CombatHUD/Run Away Button")
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	#set mouse cursor to combat crosshairs
 	Input.set_custom_mouse_cursor(crosshair)
+	#connect button signals
+	surrenderButton.connect(" pressed",self, "on_surrender_pressed")
+	runButton.connect("pressed",self, "on_run_pressed")
+	pass
+
+func on_surrender_pressed():
+	#TODO:
+	#check how badly the enemy robs the player
+	#check how badly the enemy maims the player and whether or not they kill them
+	print("Uncle, uncle, uncle")
+	pass
+	
+func on_run_pressed():
+	#TODO:
+	#check whether or not the player gets away
+	#check whether or not the player takes damage, and how bad the damage is
+	print("Ruuuun, Forrest, ruuuun")
 	pass
 
 func _pause():
