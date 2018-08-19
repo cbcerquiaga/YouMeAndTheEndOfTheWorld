6e851393eacc004a9d0190f7daa9d3aa9b917de9
Daniel Zwiener <17934820+UngratefulFool@users.noreply.github.com>
2018-08-18 22:20:34 -0500

    Refactoring and cleaning up sceneManager.gd

--- You, Me, and the End of the World/Scripts/sceneManager.gd
+++ You, Me, and the End of the World/Scripts/sceneManager.gd
@@ -1,20 +1,15 @@
 extends Control
 
-const marginForCameraToZoomOut = 300
-#const slowdownOfZoomOut = 5
-const maxZoomOutScale = 0.7
-const minZoomInScale = 1.0
-
 # class member variables go here, for example:
 var isp1Playing
 var isp2Playing
 var exploreMouse = load("res://Images/ExploreCursor.png")
 
 #Timers handle the popup cooldown, it is required to not spam
-var timer1 = Timer.new()
-var timer2 = Timer.new()
-var cooldown = false
-var cooldown2 = false
+var dropoutCoolDownPlayer1 = Timer.new()
+var dropoutCoolDownPlayer2 = Timer.new()
+var dropoutCooldownPlayer1 = false
+var dropoutCooldownPlayer2 = false
 
 #Handles inventory popup, cooldown is required to not spam the screen
 var invPopupTimerP1 = Timer.new()
@@ -32,13 +27,13 @@ func _ready():
 	isp2Playing = true
 	Input.set_custom_mouse_cursor(exploreMouse)
 	#Handling dropout Timers
-	timer1.connect("timeout", self, "_on_timer_timeout" )
-	timer2.connect("timeout", self, "_on_timer2_timeout")
-	add_child(timer1)
-	add_child(timer2)
+	dropoutCoolDownPlayer1.connect("timeout", self, "_on_timer_timeout" )
+	dropoutCoolDownPlayer2.connect("timeout", self, "_on_timer2_timeout")
+	add_child(dropoutCoolDownPlayer1)
+	add_child(dropoutCoolDownPlayer2)
 	_start_timer1()
 	_start_timer2()
-	
+
 	#Handles Inventory popup timer
 	invPopupTimerP1.connect("timeout", self, "_player1_Inventory_cooldown_reset")
 	invPopupTimerP2.connect("timeout", self, "_player2_Inventory_cooldown_reset")
@@ -46,64 +41,65 @@ func _ready():
 	add_child(invPopupTimerP2)
 	_start_p1_Inventory_Cooldown()
 	_start_p2_Inventory_Cooldown()
-	
+
 
 	#Handling Camera
 	update_camera()
 
 #Function to start the timer at 1 seconds
 func _start_timer1():
-	if !timer1.time_left > 0:
-		timer1.wait_time = 1
-		timer1.start() #to start
+	if !dropoutCoolDownPlayer1.time_left > 0:
+		dropoutCoolDownPlayer1.wait_time = 1
+		dropoutCoolDownPlayer1.start() #to start
 
 #Function to start the second timer at 1 seconds
 func _start_timer2():
-	if !timer2.time_left > 0:
-		timer2.wait_time = 1
-		timer2.start() #to start
+	if !dropoutCoolDownPlayer2.time_left > 0:
+		dropoutCoolDownPlayer2.wait_time = 1
+		dropoutCoolDownPlayer2.start() #to start
 
 #The function to handle when the timer times out
 func _on_timer_timeout():
-	timer1.stop()
-	cooldown = false
+	dropoutCoolDownPlayer1.stop()
+	dropoutCooldownPlayer1 = false
 
 #The function to handle when the second time times out
 func _on_timer2_timeout():
-	timer2.stop()
-	cooldown2 = false
+	dropoutCoolDownPlayer2.stop()
+	dropoutCooldownPlayer2 = false
 
-#Function to start the timer at 1 seconds
+#Function to start the inventory Popup Timer for player1 at 1 second
 func _start_p1_Inventory_Cooldown():
 	if !invPopupTimerP1.time_left > 0:
 		invPopupTimerP1.wait_time = 1
 		invPopupTimerP1.start() #to start
 		player1InventoryPopupCooldown = true
 
-#Function to start the second timer at 1 seconds
+#Function to start the inventory Popup Timer for player2 at 1 second
 func _start_p2_Inventory_Cooldown():
 	if !invPopupTimerP2.time_left > 0:
 		invPopupTimerP2.wait_time = 1
 		invPopupTimerP2.start() #to start
 		player2InventoryPopupCooldown = true
 
+#Is called each time the invPopupTimerP1 runs out of time
 func _player1_Inventory_cooldown_reset():
 	invPopupTimerP1.stop()
 	player1InventoryPopupCooldown = false
-	
+
+#Is called each time the invPopupTimerP2 runs out of time
 func _player2_Inventory_cooldown_reset():
 	invPopupTimerP2.stop()
 	player2InventoryPopupCooldown = false
 
+#Handles pausing
 func _pause():
 	get_tree().paused = true
 	var pause_popup = get_node("/root/Root/HUDControl/pause_popup")
-#	$pause_popup.rect_global_position = (-(player1.global_position + player2.global_position)/(2) + (screensize/(2)))
 	var transform = get_viewport_transform()
-	print(transform)
-#	$pause_popup.popup(Rect2(transform.x, transform.y))
 	pause_popup.show()
-	
+
+#Handles showing/hiding player2's inventory and freezing the player
 func _inventory1():
 	#TODO: adjust position to the right half of the screen if both players are active, or put in the middle if only one is
 	var inventoryScreenP1 = get_node("/root/Root/HUDControl/InventoryScreenP1")
@@ -114,6 +110,7 @@ func _inventory1():
 		inventoryScreenP1.hide()
 	player1.isFrozen = !player1.isFrozen
 
+#Handles showing/hiding player2's inventory and freezing the player
 func _inventory2():
 	#TODO: adjust position to the right half of the screen if both p
 	var inventoryScreenP2 = get_node("/root/Root/HUDControl/InventoryScreenP2")
@@ -125,42 +122,51 @@ func _inventory2():
 	player2.isFrozen = !player2.isFrozen
 
 func _process(delta):
-	if Input.is_action_pressed("p1_dropout") and !cooldown:
+
+	#Handles player1 dropping out
+	#Player1 can dropout so long as cooldown isn't active, and player2 is playing
+	if Input.is_action_pressed("p1_dropout") and !dropoutCooldownPlayer1:
 		if(isp1Playing && isp2Playing):
-			cooldown = true
+			dropoutCooldownPlayer1 = true
 			isp1Playing = false
 			_start_timer1()
 		elif(!isp1Playing):
-			cooldown = true
+			dropoutCooldownPlayer1 = true
 			isp1Playing = true
 			_start_timer1()
-	if Input.is_action_pressed("p2_dropout") and !cooldown2:
+
+	#Handles player2 dropping out
+	#Player2 can dropout so long as cooldown2 isn't active, and player1 is playing
+	if Input.is_action_pressed("p2_dropout") and !dropoutCooldownPlayer2:
 		if(isp1Playing && isp2Playing):
-			cooldown2 = true
+			dropoutCooldownPlayer2 = true
 			isp2Playing = false
 			_start_timer2()
 		elif(!isp2Playing):
-			cooldown2 = true
+			dropoutCooldownPlayer2 = true
 			isp2Playing = true
 			_start_timer2()
-	
+
+	#Handle pausing
 	if Input.is_action_pressed("pause"):
 		_pause()
+	#Handles the UI for the inventory for player1
 	if Input.is_action_pressed("p1_inventory") and !player1InventoryPopupCooldown:
 		_inventory1()
 		_start_p1_Inventory_Cooldown()
+	#Handles the UI for the inventory for player2
 	if Input.is_action_pressed("p2_inventory") and !player2InventoryPopupCooldown:
 		_inventory2()
 		_start_p2_Inventory_Cooldown()
-		pass
 	pass
 
+#function called whenever the camera should be adjusted
 func update_camera():
 #	#Get the current canvas transform
 	var canvas_transform = get_viewport().get_canvas_transform()
-	
+
 #	#Update the canvas with the center of the two players being the center of the viewport
 	canvas_transform[2] = (-(player1.global_position + player2.global_position)/(2) + (screensize/(2)))
-	
+
 	#Applies the transform
 	get_viewport().set_canvas_transform(canvas_transform)
