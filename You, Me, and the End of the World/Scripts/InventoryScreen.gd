b0d1441384384cdb78bd06d416044efe8646cbcb
Blake Erquiaga <cbcerquiaga@me.com>
2018-08-20 12:35:07 -0500

    Arrow keys allow switching tabs on inventory
    Started to work on adding text also, that's next. Progress on #85

--- You, Me, and the End of the World/Scripts/InventoryScreen.gd
+++ You, Me, and the End of the World/Scripts/InventoryScreen.gd
@@ -6,6 +6,9 @@ var C
 var R
 var highlight
 var currentFrame
+var currentTab #stores text for current tab to make it easier to code other nodes
+onready var tabIndex = 0
+onready var tabs = ["quests","weapons","equippable","consumable","misc","map"]
 onready var leftButton = get_node("Status Box/Left Button")
 onready var rightButton = get_node("Status Box/Right Button")
 onready var questButton = get_node("Tabs/questButton")
@@ -19,6 +22,7 @@ func _ready():
 	#self.queue_free()
 	justOpened = true
 	currentFrame = 0
+	currentTab = tabs[tabIndex]
 	L = get_node("Status Box/Left Icon")
 	C = get_node("Status Box/Center Icon")
 	R = get_node("Status Box/Right Icon")
@@ -77,35 +81,66 @@ func rightButtonPressed():
 		
 func questButtonPressed():
 	highlight.set_position(Vector2(0,0))
+	tabIndex = 0
 	#TODO: change text in inventory section
 	
 func weaponButtonPressed():
 	highlight.set_position(Vector2(55,0))
+	tabIndex = 1
 	#TODO: change text in inventory section
 	
 func equippableButtonPressed():
 	highlight.set_position(Vector2(110,0))
+	tabIndex = 2
 	#TODO: change text in inventory section
 	
 func consumableButtonPressed():
 	highlight.set_position(Vector2(165,0))
+	tabIndex = 3
 	
 func miscButtonPressed():
 	highlight.set_position(Vector2(220,0))
+	tabIndex = 4
 	#TODO: change text in inventory section
 	
 func mapButtonPressed():
 	highlight.set_position(Vector2(275,0))
+	tabIndex = 5
 	#TODO: change text in inventory section
 
 func _process(delta):
 	#TODO: add actual functionality to keybinds
+	currentTab = tabs[tabIndex]
 	if Input.is_action_just_pressed("p1_move_up"):
 		print("Scroll up")
 	if Input.is_action_just_pressed("p1_move_down"):
 		print("Scroll down")
 	if Input.is_action_just_pressed("p1_move_left"):
-		print("Tab over left")
+		#print("Tab over left")
+		if currentTab == "quests":
+			mapButtonPressed()
+		elif currentTab == "map":
+			miscButtonPressed()
+		elif currentTab == "misc":
+			consumableButtonPressed()
+		elif currentTab == "consumable":
+			equippableButtonPressed()
+		elif currentTab == "equippable":
+			weaponButtonPressed()
+		else: #currentTab == "weapons":
+			questButtonPressed()
 	if Input.is_action_just_pressed("p1_move_right"):
-		print("Tab over right")
+		#print("Tab over right")
+		if currentTab == "quests":
+			weaponButtonPressed()
+		elif currentTab == "weapons":
+			equippableButtonPressed()
+		elif currentTab == "equippable":
+			consumableButtonPressed()
+		elif currentTab == "consumable":
+			miscButtonPressed()
+		elif currentTab == "misc":
+			mapButtonPressed()
+		else: #currentTab == "map"
+			questButtonPressed()
 	pass
---
+++ You, Me, and the End of the World/Scripts/ItemsText.gd
@@ -0,0 +1,14 @@
+extends RichTextLabel
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	#text = get_node("res://tscn files/player1.tscn")
+	pass
