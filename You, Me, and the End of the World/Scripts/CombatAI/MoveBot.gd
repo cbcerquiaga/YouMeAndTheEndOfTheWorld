2563bbb827216b38ad52d678daa7dd48553c6fa6
Blake Erquiaga <cbcerquiaga@me.com>
2018-09-02 23:37:18 -0500

    Started MoveBot
    Also added some perk ideas

--- You, Me, and the End of the World/Scripts/CombatAI/MoveBot.gd
+++ You, Me, and the End of the World/Scripts/CombatAI/MoveBot.gd
@@ -1,12 +1,30 @@
 extends "res://Scripts/CombatAI/CombatEnemy.gd"
 
+var maxDistance
+var minDistance
+
 func _ready():
+	maxDistance = 800
+	minDistance = 600
 	pass
 
 func jump():
 	pass
 
 func dodge():
+	var distanceToPlayer = (self.global_position - player.global_position).length()
+	if distanceToPlayer >= 0: #player is on the left
+		print("To the left to the left")
+		if distanceToPlayer < minDistance: #need to move away
+			print("get away from me!")
+		elif distanceToPlayer > maxDistance: #need to move closer
+			print("get over here!")
+	else:
+		print("To the right to the right")
+		if (0 - distanceToPlayer) < minDistance:
+			print("get away from me!")
+		elif (0 - distanceToPlayer) > maxDistance:
+			print("get over here!")
 	#Four options:
 		#Move forward
 		#Move back
