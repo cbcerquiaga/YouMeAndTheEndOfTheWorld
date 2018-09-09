5fecce2f2a692df58ba9a0f43b4760e632da7288
Blake Erquiaga <cbcerquiaga@me.com>
2018-09-09 10:28:04 -0500

    Minor changes to moveBot

--- You, Me, and the End of the World/Scripts/CombatAI/MoveBot.gd
+++ You, Me, and the End of the World/Scripts/CombatAI/MoveBot.gd
@@ -6,8 +6,8 @@ onready var player = get_node("../CombatPlayer")
 onready var force = Vector2(0, GRAVITY)
 
 func _ready():
-	maxDistance = 100
-	minDistance = 80
+	maxDistance = 655
+	minDistance = 60
 	pass
 
 func jump():
@@ -21,28 +21,31 @@ func _physics_process(delta):
 	velocity += force * delta
 	# Integrate velocity into motion and move
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+	print(str(velocity))
 	lastFrameEndSpeed = velocity
 	pass
 
 func dodge(delta):
-	var distanceToPlayer = (self.global_position - player.global_position).x
+	var distanceToPlayer = (self.global_position.x - player.global_position.x)
 	if distanceToPlayer >= 0: #player is on the left
-		print("To the left to the left")
-		if distanceToPlayer < minDistance: #need to move away
-			print("get away from me!")
+		#print("To the left to the left")
+		if distanceToPlayer < maxDistance and distanceToPlayer > minDistance:
+			stopWalking(delta)
+		elif distanceToPlayer < minDistance: #need to move away
+			#print("get away from me!")
 			walkRight()
 		elif distanceToPlayer > maxDistance: #need to move closer
-			print("get over here!")
+			#print("get over here!")
 			walkLeft()
-		else:
-			stopWalking(delta)
 	else:
-		print("To the right to the right")
-		if (0 - distanceToPlayer) < minDistance:
-			print("get away from me!")
+		#print("To the right to the right")
+		if distanceToPlayer < maxDistance and distanceToPlayer > minDistance:
+			stopWalking(delta)
+		elif (0 - distanceToPlayer) < minDistance:
+			#print("get away from me!")
 			walkLeft()
 		elif (0 - distanceToPlayer) > maxDistance:
-			print("get over here!")
+			#print("get over here!")
 			walkRight()
 		else:
 			stopWalking(delta)
@@ -63,9 +66,10 @@ func walkRight():
 			force.x += WALK_FORCE
 
 func stopWalking(delta):
-		var vsign = sign(velocity.x)
-		var vlen = abs(velocity.x)
-		vlen -= STOP_FORCE * delta
-		if vlen < 0:
-			vlen = 0
-		velocity.x = vlen * vsign
+	var vsign = sign(velocity.x)
+	var vlen = abs(velocity.x)
+	vlen -= STOP_FORCE * delta
+	if vlen < 0:
+		vlen = 0
+	velocity.x = vlen * vsign
+	#print("STOP!")
