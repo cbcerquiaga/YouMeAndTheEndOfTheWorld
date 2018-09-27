1244f0f6ccf635429650ccefa29fd441161a6347
Blake Erquiaga <cbcerquiaga@me.com>
2018-09-27 10:21:11 -0500

    Enemy in exploration world now sends player to combat scene
    We need to have something pop up over an enemy's head to warn them to avoid this NPC or risk combat, a pop-up that needs to be dealt with before the combat actually starts, and a way to have enemies shoot and first-strike players

--- You, Me, and the End of the World/Scripts/WorldEnemy.gd
+++ You, Me, and the End of the World/Scripts/WorldEnemy.gd
@@ -7,10 +7,14 @@ func _ready():
 	pass
 
 func _process(delta):
-	if((self.global_position - player1.global_position).length() < 300):
-		self.move_and_slide(player1.global_position - self.global_position)
-	elif((self.global_position - player2.global_position).length() < 300):
-		self.move_and_slide(player2.global_position - self.global_position)
+	#playerrs are not within combat range
+	if ((self.global_position - player1.global_position).length() >= 100) and ((self.global_position - player2.global_position).length() >= 100):
+		if((self.global_position - player1.global_position).length() < 300):
+			self.move_and_slide(player1.global_position - self.global_position)
+		elif((self.global_position - player2.global_position).length() < 300):
+			self.move_and_slide(player2.global_position - self.global_position)
+	else: #players are within combat range
+		handle_collide(player1)
 	pass
 
 func handle_collide(player):
