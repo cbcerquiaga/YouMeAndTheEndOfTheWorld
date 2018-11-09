fb0702dc760c17f2d8a97adcc84effa1a3c5538e
Blake Erquiaga <cbcerquiaga@me.com>
2018-11-09 09:55:51 -0500

    Started working on getting fishing scene to scroll
    I extended the scene itself, added a camera controlling kinematic body just like what we did for the exploration scene, added some pseudocode, and moved the fish emitter script to the scripts folder

---
+++ You, Me, and the End of the World/Scripts/Fishing.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	pass
+
+func _process(delta):
+#if one player catches a fish but the other hasn't, disable its movement and let it get off screen
+#if both players catch a fish or they reach the bottom, start scrolling up
+#if a player strikes an obstacle, delete them from the scene
+	pass
---
+++ You, Me, and the End of the World/Scripts/FishingPlayerTracker.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+onready var player1 = get_node("/root/TileMap/Player1")
+onready var player2 = get_node("/root/TileMap/Player2")
+var hasReachedBottom = false
+var scrollSpeed = 100
+var maxScrollSpeed = 150
+var minScrollSpeed = 50
+
+func _ready():
+#	$Camera.zoom = Vector2(1.0, 1.0)
+	pass
+
+func _process(delta):
+#if !hasReachedBottom: #time to scroll down
+	#check if one of the players is at the bottom of the screen
+	#if player1.y - camera.y > lowerLimit and player2.y - camera.y > upperLimit
+	#or if player2.y - camera.y > lowerLimit and player1.y - camera.y > upperLimit
+	#scroll down at maxScrollSpeed
+	#elif player1.y - camera.y < upperLimit and player2.y - camera.y < upperLimit
+	#or if player2.y - camera.y < upperLimit and player1.y - camera.y < upperLimit
+	#scroll down at minScrollSpeed
+	#else
+	#scroll at scrollSpeed
+#else:
+	#scroll up at maxScrollSpeed
+	pass
