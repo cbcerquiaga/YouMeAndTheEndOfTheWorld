ab3849aedfcf8b4edd06f0e7b52710ca5bbf4bf0
UngratefulFool <dzwiener4@me.com>
2018-11-16 21:39:50 -0600

    Add gameover conditions to fishing scene

--- You, Me, and the End of the World/Scripts/FishingPlayerTracker.gd
+++ You, Me, and the End of the World/Scripts/FishingPlayerTracker.gd
@@ -6,6 +6,7 @@ onready var bottomDetection = get_node("BottomDetection")
 
 var hasReachedBottom = false
 var scrollSpeed = 100
+var gameOver = false
 
 func _ready():
 	$Camera2D.zoom = Vector2(1.5, 1.5)
@@ -17,8 +18,13 @@ func _process(delta):
 		for i in bodies:
 			if i.name == "BottomCheck":
 				hasReachedBottom = true
-	if !hasReachedBottom: #time to scroll down
-		self.move_and_slide(Vector2(0, scrollSpeed))
-	else: #time to scroll up
-		self.move_and_slide(Vector2(0, -scrollSpeed))
+			if i.name == "TopCheck" and hasReachedBottom:
+				#GAME OVER CONDITIONS
+				gameOver = true
+				print("Game over")
+	if !gameOver:
+		if !hasReachedBottom: #time to scroll down
+			self.move_and_slide(Vector2(0, scrollSpeed))
+		else: #time to scroll up
+			self.move_and_slide(Vector2(0, -scrollSpeed))
 	pass
