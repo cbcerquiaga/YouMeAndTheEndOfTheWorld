bdbb285fedb5cce96a07e49cc95cec232d829cd5
Blake Erquiaga <cbcerquiaga@me.com>
2019-01-13 22:10:51 -0600

    Worked on fish emitter in fishing scene
    Made some progress towards a functional algorithm for randomly generating and placing fish

--- You, Me, and the End of the World/Scripts/FishEmitter.gd
+++ You, Me, and the End of the World/Scripts/FishEmitter.gd
@@ -1,20 +1,33 @@
 extends Node2D
 
+onready var availableFish = ["bass", "trout", "eel", "mutant", "gold", "minnow"]
+onready var fish = load("res://Scripts/BasicFish.gd")
+var maxDepth = 100 #default maximum depth unless a new one is given
 
 func _ready():
 	#figure out how many fish to instance
-	var numFish = rand_range(0, 30)
-	#figure out what kind of fish to spawn
-	#randomly place those fish both horizontally and in terms of depth
+	var numFish = int(round(rand_range(0, 30)))
+	for i in range (0, numFish):
+		var fishType = chooseFishType()
+		spawnFish(fishType) #figure out what kind of fish to spawn
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func setMaxDepth(number):
+	maxDepth = number
 
 func chooseFishType():
+	var index = rand_range(0, availableFish.size())
+	return availableFish[index]
 	pass
 	
-func spawnFish():
+func spawnFish(type):
+	var depth = rand_range(0, maxDepth)
+	var width = rand_range(0, 50)#width of the scene area
+	var thisFish = fish.instance()
+	thisFish.set_position(Vector2(width, depth))
+	self.get_parent().add_child(thisFish)
+	#var sprite = get_sprite(type) #TODO: get assets and make an actual call to change the sprite
 	pass
+
+func changeAvailableFish(newArray):
+	availableFish = newArray
