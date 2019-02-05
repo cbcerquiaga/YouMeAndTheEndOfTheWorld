b9b18fad3786204b0133974b8f6c187d6e81a6d1
Blake Erquiaga <cbcerquiaga@me.com>
2019-02-04 21:48:40 -0600

    Worked on equipping tools in farming scene
    #195 #194 #193 #192 so far I've only got the hoe in the scene. It "equips" properly but doesn't follow the player.

---
+++ You, Me, and the End of the World/Scripts/Farming.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+onready var player1 = get_node("TileMap/FarmingPlayer1")
+onready var player2 = get_node("TileMap/FarmingPlayer2")
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+	
+func getClosestTool(player):
+	if player == 1:
+		if player1.position.x < 270 or player1.position.x > 760 or player1.position.y > 160:
+			return "none"
+		#elif position closest to seedbag
+		#elif position closest to watering can
+		#elif position closest to basket
+		else: #closest to hoe
+			return "hoe"
+	else: #if player == 2:
+		if player2.position.x < 270 or player2.position.x > 760 or player2.position.y > 160:
+			return "none"
+		#elif position closest to seedbag
+		#elif position closest to watering can
+		#elif position closest to basket
+		else: #closest to hoe
+			return "hoe"
+			
+
+func _process(delta):
+	#player 1 wants to pick up or swap tools
+	if Input.is_action_just_pressed("p1_action2"):
+		#check if the player is close enough to any tools to do this
+		var closestTool = getClosestTool(1)
+		if closestTool == "hoe":
+			get_node("ToolBench/hoe").equip(1)
+	pass
--- You, Me, and the End of the World/Scripts/FarmingTool.gd
+++ You, Me, and the End of the World/Scripts/FarmingTool.gd
@@ -9,6 +9,8 @@ onready var player2 = get_node("TileMap/FarmingPlayer2")
 onready var toolBench = get_node("ToolBench")
 var toolBenchX = 0 #adjust these depending on which tool this is
 var toolBenchY = 0
+var goalPosition = Vector2()
+var motion = Vector2()
 	
 func updateFacingRight(boolean):
 	isFacingRight = boolean
@@ -21,28 +23,38 @@ func updateFacingRight(boolean):
 			
 func equip(player):
 	equippedPlayer = player
-	if equippedPlayer == 1:
-		actionKey = is_action_just_pressed("p1_action1")
-	elif equippedPlayer == 2:
-		actionKey = is_action_just_pressed("p2_action1")
+	print("you're holding the tool")
+#	if equippedPlayer == 1:
+#		actionKey = is_action_just_pressed("p1_action1")
+#	elif equippedPlayer == 2:
+#		actionKey = is_action_just_pressed("p2_action1")
 	
 func unEquip():
 	equippedPlayer = 0
 	
 func _process():
 	if equippedPlayer == 1: #follow player 1
-		position.y = player1.position.y
+		goalPosition.y = player1.position.y
 		if isFacingRight:
-			position.x = player1.position.x + 20
+			goalPosition.x = player1.position.x + 20
 		else:
-			position.x = player1.position.x - 20
+			goalPosition.x = player1.position.x - 20
 	elif equippedPlayer == 2: #follow player 2
-		position.y = player2.position.y
+		goalPosition.y = player2.position.y
 		if isFacingRight:
-			position.x = player2.position.x + 20
+			goalPosition.x = player2.position.x + 20
 		else:
-			position.x = player2.position.x - 20
+			goalPosition.x = player2.position.x - 20
 	else: #go to position on the tool bench
-		position.x = toolBench.position.x + toolBenchX
-		position.y = toolBench.position.y + toolBenchY
+		goalPosition.x = toolBench.position.x + toolBenchX
+		goalPosition.y = toolBench.position.y + toolBenchY
+	if position.x > goalPosition.x:
+		motion.x = player1.SPEED
+	elif position.x < goalPosition.x:
+		motion.x = 0 - player1.SPEED
+	if position.y > goalPosition.y:
+		motion.y = player1.SPEED
+	elif position.y < goalPosition.y:
+		motion.y = 0 - player1.SPEED
+	move_and_slide(motion)
 	pass
