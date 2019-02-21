da0126581ee8ad0978f5136223f67d3daedff3af
Blake Erquiaga <cbcerquiaga@me.com>
2019-02-21 10:33:30 -0500

    Added watering can to the farming scene
    #193

---
+++ You, Me, and the End of the World/Scripts/wateringCan.gd
@@ -0,0 +1,11 @@
+extends "res://Scripts/FarmingTool.gd"
+
+func _ready():
+	toolBenchX = 543.728516
+	toolBenchY = 38.226074
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
