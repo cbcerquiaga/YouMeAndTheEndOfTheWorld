780a394d15cda509f88ea14f79be596ef3b52a62
Blake Erquiaga <cbcerquiaga@me.com>
2019-02-27 10:40:49 -0500

    Added basket and harvest barrel to farming scene
    #195

---
+++ You, Me, and the End of the World/Scripts/basket.gd
@@ -0,0 +1,11 @@
+extends "res://Scripts/FarmingTool.gd"
+
+func _ready():
+	toolBenchX = 627
+	toolBenchY = 37
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
