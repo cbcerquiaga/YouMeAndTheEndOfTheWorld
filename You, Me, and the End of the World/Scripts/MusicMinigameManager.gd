9f73e38bef522d457b5a9f7654d280b6e7b59c26
Blake Erquiaga <cbcerquiaga@me.com>
2019-05-26 14:52:32 -0400

    Added player 2 and score labels to music minigame
    It needs some polish, but it's basically done now

--- You, Me, and the End of the World/Scripts/MusicMinigameManager.gd
+++ You, Me, and the End of the World/Scripts/MusicMinigameManager.gd
@@ -3,8 +3,10 @@ extends Node
 #these keep track of the current and next key presses for each player
 var tempo = 100 #time between note spawns
 var time #tracks time for tempo
-var song #the actual song being played
-var currentNote = 0 #index in the song where we are now
+var song1 #the actual song being played for player 1
+var song2 #song for player 2
+var currentNote1 = 0 #index in the song where p1 is now
+var currentNote2 = 0 #index for player 2
 #var loops = 2 #number of times we loop through the song before completion
 var defaultSong = ["L","C","L","R","C","R"] #default song if song isn't changed
 var p1Score = 0
@@ -15,12 +17,16 @@ onready var p1Pos = "L"
 onready var p2Pos = "C"
 onready var note = load("res://tscn files/MusicKey.tscn")
 onready var player1 = get_node("player1")
-#onready var player2 = get_node("player2")
+onready var player2 = get_node("player2")
 var notes #array of all notes
+#HUD elements
+onready var scoreLabel1 = get_node("HUD/p1ScoreLabel")
+onready var scoreLabel2 = get_node("HUD/p2ScoreLabel2")
 
 func _ready():
 	time = 0
-	song = defaultSong
+	song1 = defaultSong
+	song2 = ["L", "L", "L", "R", "R", "R", "L", "R"]
 	notes = []
 	pass
 
@@ -41,10 +47,10 @@ func p1Left():
 func p2Left():
 	if p2Pos == "C":
 		p2Pos = "L"
-		#change x to left
+		player2.position.x = 110
 	elif p2Pos == "R":
 		p2Pos = "C"
-		#change x to center
+		player2.position.x = 210
 		
 func p1Right():
 	if p1Pos == "C":
@@ -57,20 +63,20 @@ func p1Right():
 func p2Right():
 	if p2Pos == "C":
 		p2Pos = "R"
-		#change x to right
+		player2.position.x = 310
 	elif p2Pos == "L":
 		p2Pos = "C"
-		#change x to center
+		player2.position.x = 210
 	
-func initNote(spawnPos):
-	print("initialize it, do it now! " + str(spawnPos))
+func initNote1(spawnPos):
+	#print("initialize it, do it now! " + str(spawnPos))
 	var tempNote = note.instance()
 	tempNote.position = spawnPos
 	get_node("noteEmitter").add_child(tempNote)
 	notes.append(tempNote)
 	pass
 	
-func spawnNoteAtSide(side):
+func spawnNoteAtSide1(side):
 	var spawnPos = Vector2()
 	spawnPos.y = 50
 	if side == "L":
@@ -79,17 +85,47 @@ func spawnNoteAtSide(side):
 		spawnPos.x = 730
 	else: #if side == "R":
 		spawnPos.x = 830
-	initNote(spawnPos)
+	initNote1(spawnPos)
 	
-func spawnNote():
+func spawnNote1():
 	#print("current note: " + str(currentNote))
-	if currentNote < song.size() - 1:
-		currentNote = currentNote + 1
+	if currentNote1 < song1.size() - 1:
+		currentNote1 = currentNote1 + 1
 	else:
-		currentNote = 0
-	var side = song[currentNote]
+		currentNote1 = 0
+	var side = song1[currentNote1]
 	#print("side: " + str(side))
-	spawnNoteAtSide(side)
+	spawnNoteAtSide1(side)
+	pass
+	
+func initNote2(spawnPos):
+	#print("initialize it, do it now! " + str(spawnPos))
+	var tempNote = note.instance()
+	tempNote.position = spawnPos
+	get_node("noteEmitter").add_child(tempNote)
+	notes.append(tempNote)
+	pass
+	
+func spawnNoteAtSide2(side):
+	var spawnPos = Vector2()
+	spawnPos.y = 50
+	if side == "L":
+		spawnPos.x = 110 #0
+	elif side == "C":
+		spawnPos.x = 210
+	else: #if side == "R":
+		spawnPos.x = 310
+	initNote2(spawnPos)
+	
+func spawnNote2():
+	#print("current note: " + str(currentNote))
+	if currentNote2 < song2.size() - 1:
+		currentNote2 = currentNote2 + 1
+	else:
+		currentNote2 = 0
+	var side = song2[currentNote2]
+	#print("side: " + str(side))
+	spawnNoteAtSide2(side)
 	pass
 
 func _process(delta):
@@ -109,7 +145,8 @@ func _process(delta):
 	else:
 		time = 0
 		#print("it's time")
-		spawnNote()
+		spawnNote1()
+		spawnNote2()
 	if notes.size() > 0:
 		for i in range (0, notes.size() - 1):
 			var nextNote = notes[i]
@@ -123,7 +160,7 @@ func _process(delta):
 				p2streak = 0
 				notes.remove(i)
 				nextNote.destroy(false)
-			elif nextNote.overlaps_area(player1):
+			if nextNote.overlaps_area(player1):
 				p1streak = p1streak + 1
 				if p1streak > 30:
 					p1Score = p1Score + 10
@@ -135,15 +172,19 @@ func _process(delta):
 					p1Score = p1Score + 1
 				notes.remove(i)
 				nextNote.destroy(true)
-#			elif player2.overlaps_area(nextNote):
-#				p2streak = p2streak + 1
-#				if p2streak > 30:
-#					p2Score = p1Score + 10
-#				elif p2streak > 20:
-#					p2Score = p1Score + 5
-#				elif p2streak > 10:
-#					p2Score = p2Score + 2
-#				else:
-#					p2Score = p2Score + 1
-			print(str(p1Score))
+			if nextNote.overlaps_area(player2):
+				p2streak = p2streak + 1
+				if p2streak > 30:
+					p2Score = p2Score + 10
+				elif p2streak > 20:
+					p2Score = p2Score + 5
+				elif p2streak > 10:
+					p2Score = p2Score + 2
+				else:
+					p2Score = p2Score + 1
+				notes.remove(i)
+				nextNote.destroy(true)
+			scoreLabel1.text = str(p1Score)
+			scoreLabel2.text = str(p2Score)
+			#print(str(p1Score))
 	pass
