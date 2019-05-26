b1968be997ed184de522cc0ee71c074e10001b09
Blake Erquiaga <cbcerquiaga@me.com>
2019-05-26 19:38:49 -0400

    Added basketball minigame scene
    Since I had the new idea for how to do this minigame today, may as well add the scene now even if we don't work on it for awhile

---
+++ You, Me, and the End of the World/Scripts/basketballLiteralBall.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.set_bounce(.7)
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
