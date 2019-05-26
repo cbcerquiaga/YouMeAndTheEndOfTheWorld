04bb8d97e9278fa9efd236548b06c0c8a5afbd7d
Blake Erquiaga <cbcerquiaga@me.com>
2019-05-26 10:41:14 -0400

    Worked on music minigame
    #186 instead of doing it guitar-hero style, I changed it so that the player is only moving to try and catch notes, which I think will be more unique and easier to implement. I've started working on that, but I can't seem to get notes to appear for some reason, I'll work on that next

--- You, Me, and the End of the World/Scripts/MusicKey.gd
+++ You, Me, and the End of the World/Scripts/MusicKey.gd
@@ -3,14 +3,12 @@ extends KinematicBody2D
 onready var speed = 20
 onready var sprite = get_node("Sprite")
 var key
-var assignedPlayer
-var isLead = false
 onready var waiting
 
 func _ready():
 	set_process(true)
 	sprite.visible = true
-	waiting = true
+	waiting = false
 	pass
 	
 func changeSpeed(newSpeed):
@@ -21,61 +19,21 @@ func wait():
 	
 func go():
 	waiting = false
-	
-func setLead():
-	isLead = true
 
-#changes the sprite depending on which player and what value is passed
-func changeTexture(value, player):
-	if player == 2:
-		assignedPlayer = 2
-		if value == "A":
-			sprite.frame = 0
-			key = "A"
-		elif value == "D":
-			sprite.frame = 1
-			key = "D"
-		elif value == "S":
-			sprite.frame = 2
-			key = "S"
-		else: #value == "W":
-			sprite.frame = 3
-			key = "W"
-	else: #if player == 1:
-		assignedPlayer = 1
-		if value == "A":
-			sprite.frame = 4
-			key = "A"
-		elif value == "D":
-			sprite.frame = 5
-			key = "D"
-		elif value == "S":
-			sprite.frame = 6
-			key = "S"
-		else: #value == "W":
-			sprite.frame = 7
-			key = "W"
-			
+
 func drestroy(timing):
 	#playKillAnimation(timing)
 	queue_free()
 	
 func playKillAnimation(timing):
-	if timing == "too soon":
-		#play the too soon animation
-		pass
-	elif timing == "too late":
-		#play the too late animation
-		pass
-	elif timing == "just right":
-		#play the just right animation
+	if timing == true:
+		#play happy you did it animation
 		pass
 	else:
-		#no animation
+		#play sad you screwed up animation
 		pass
 
 func _process(delta):
 	if !waiting:
 		move_and_slide(Vector2(0,speed))
-	#changeTexture("D",1)
 	pass
--- You, Me, and the End of the World/Scripts/MusicMinigameManager.gd
+++ You, Me, and the End of the World/Scripts/MusicMinigameManager.gd
@@ -1,22 +1,25 @@
 extends Node
 
 #these keep track of the current and next key presses for each player
-var keyArray1 = []
-var keyArray2 = []
-var tempo = 10
-var time
-var song
-var defaultSong = ["A","D","A","D","W","S"]
-onready var emitter1 = get_node("p1Emitter")
-onready var emitter2 = get_node("p2Emitter")
-onready var arranger1 = get_node("p1Arranger")
+var tempo = 10 #time between note spawns
+var time #tracks time for tempo
+var song #the actual song being played
+var currentNote = 0 #index in the song where we are now
+#var loops = 2 #number of times we loop through the song before completion
+var defaultSong = ["L","C","L","R","C","R"] #default song if song isn't changed
+var p1Score = 0
+var p2Score = 0
+onready var p1Pos = "L"
+onready var p2Pos = "C"
+onready var note = load("res://tscn files/MusicKey.tscn")
+onready var player1 = get_node("player1")
+#onready var player2 = get_node("player2")
+var notes #array of all notes
 
 func _ready():
 	time = 0
-	arranger1.pasSong(defaultSong)
-	#emitter1.changeSong(defaultSong)
-	#emitter2.changeSong(defaultSong)
-	
+	song = defaultSong
+	notes = []
 	pass
 
 func _pause():
@@ -24,22 +27,83 @@ func _pause():
 	get_tree().paused = true
 	#$pause_popup.update()
 	$pause_popup.show()
+	
+func p1Left():
+	if p1Pos == "C":
+		p1Pos = "L"
+		player1.position.x = 630
+	elif p1Pos == "R":
+		p1Pos = "C"
+		player1.position.x = 730
+		
+func p2Left():
+	if p2Pos == "C":
+		p2Pos = "L"
+		#change x to left
+	elif p2Pos == "R":
+		p2Pos = "C"
+		#change x to center
+		
+func p1Right():
+	if p1Pos == "C":
+		p1Pos = "R"
+		player1.position.x = 830
+	elif p1Pos == "L":
+		p1Pos = "C"
+		player1.position.x = 730
+		
+func p2Right():
+	if p2Pos == "C":
+		p2Pos = "R"
+		#change x to right
+	elif p2Pos == "L":
+		p2Pos = "C"
+		#change x to center
+	
+func initNote(spawnPos):
+	print("initialize it, do it now! " + str(spawnPos))
+	var tempNote = note.instance()
+	tempNote.position = spawnPos
+	notes.append(tempNote)
+	pass
+	
+func spawnNoteAtSide(side):
+	var spawnPos = Vector2()
+	spawnPos.y = 50
+	if side == "L":
+		spawnPos.x = 630 #0
+	elif side == "C":
+		spawnPos.x = 730
+	else: #if side == "R":
+		spawnPos.x = 830
+	initNote(spawnPos)
+	
+func spawnNote():
+	#print("current note: " + str(currentNote))
+	if currentNote < song.size() - 1:
+		currentNote = currentNote + 1
+	else:
+		currentNote = 0
+	var side = song[currentNote]
+	#print("side: " + str(side))
+	spawnNoteAtSide(side)
+	pass
 
 func _process(delta):
 	if Input.is_action_pressed("pause"):
 		_pause()
+	if Input.is_action_just_pressed("p1_move_left"):
+		p1Left()
+	elif Input.is_action_just_pressed("p1_move_right"):
+		p1Right()
 	if Input.is_action_just_pressed("p2_move_left"):
-		if keyArray1[0] == "A":
-			print("nice")
-		else:
-			print("not nice")
-	if time >= tempo:
-#		emitter1.spawnNote()
-#		keyArray1.append(emitter1.mostRecentNote)
-		#emitter2.spawnNote()
-		#keyArray2.append(emitter2.mostRecentNote)
-		time = 0
+		p2Left()
+	elif Input.is_action_just_pressed("p2_move_right"):
+		p2Right()
+	if time < tempo:
+		time = time + 1
 	else:
-		time = time+1
-		#print(str(time))
+		time = 0
+		#print("it's time")
+		spawnNote()
 	pass
