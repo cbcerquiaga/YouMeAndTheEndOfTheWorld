d05c17942930d25b570381cd37002f4801fc8d7e
Blake Erquiaga <cbcerquiaga@me.com>
2019-06-12 08:47:34 -0400

    Added 2 player puzzle ideas
    closed #206

--- You, Me, and the End of the World/Scripts/player2.gd
+++ You, Me, and the End of the World/Scripts/player2.gd
@@ -157,3 +157,24 @@ func getInventory():
 
 func getHealth():
 	playerProperty.calculateHealth()
+	
+func _dropItem(index):
+#	print("item actually dropped: " + str(index))
+	if !playerProperty.isEmpty():
+		#TODO: changed the selected item to an appropriate value
+		playerProperty.selectItemByIndex(index)
+		var item = playerProperty.getSelectedItem()
+		playerProperty.removeItem(item, "p1")
+		item.activate(self.position)
+		item.pickedUp = false
+		item.update_label()
+		
+func _drop1Item(index):
+	if !playerProperty.isEmpty():
+		#TODO: changed the selected item to an appropriate value
+		playerProperty.selectItemByIndex(index)
+		var item = playerProperty.getSelectedItem()
+		playerProperty.removeItemAmount(item, "p1", 1)
+		item.activate(self.position)
+		item.pickedUp = false
+		item.update_label()
